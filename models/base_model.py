80d56cccad4db37a97636c089d12b67b24ce122e
Lisa <110@holbetonschool.com>
2017-06-07 19:03:46 +0000

    save method done

--- models/base_model.py
+++ models/base_model.py
@@ -13,19 +13,20 @@ class BaseModel:
         """init"""
         self.id = str(uuid.uuid1())
         self.created_at = datetime.datetime.now()
+        storage.new(self)
 
     def save(self):
         """public instance method: save"""
+        """upate time and save object to json file"""
         self.updated_at = str(datetime.datetime.now())
-        j = self.to_json()
-        storage.new(j)
         storage.save()
 
     def to_json(self):
         """public instance method: to_json """
+        """creates a copy of self.__dict__ and update with atribute __class__"""
         new_dict = self.__dict__.copy()
-        new_dict.update({'__class__': str(self.__class__)})
-        self.created_at = str(self.created_at)
+        new_dict.update({'created_at': str(self.created_at)})
+        new_dict.update({'__class__': str(self.__class__.__name__)})
         return new_dict
 
     def __str__(self):
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import json
 import datetime
+
 class FileStorage:
     def __init__(self):
-        self.__file_path = 'file.json'
+        self.__file_path = './file.json'
         self.__objects = {}
 
     def all(self):
@@ -14,29 +15,22 @@ class FileStorage:
         self.__objects[obj.id] = obj
 
     def save(self):
-        new_json = {}
-        new_dict = {}
+        store = {}
         obj = self.__objects
-        for k, v in obj.items():
-            id = k
-            cls = v.__class__.__name__
-            new_dict = obj[k].__dict__
-        new_json[id] = new_dict
-        new_dict.update({'__class__': cls})
-        for k, v in new_json[id].items():
-            if isinstance(v, datetime.datetime):
-                new_dict.update({k : str(v)})
-            else:
-                new_dict.update({k : v})
-        new_json[id] = new_dict
-        with open(self.__file_path, 'w') as f:
-            json.dump(new_json, f)
+        for i in self.__objects.keys():
+            temp = self.__objects[i].to_json()
+        store[i] = temp
+        with open(self.__file_path, 'w+') as f:
+            json.dump(store, f)
 
     def reload(self):
-        with open("file.json", 'r') as f:
-            rd = json.load(f)
+        with open(self.__file_path, 'r') as f:
+            r = json.load(f)
+        from models.base_model import BaseModel
+        for i in r.keys():
+            self.__objects[i] = BaseModel(r[i])
+        return self.__objects
 
-        print("hi ---------")
-        print(rd)
-        
-        return {"hello" : "world"}
+        """
+        self.__objects[i] = eval(BaseModel)(r[i])
+        """
