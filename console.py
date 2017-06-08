ff966955e7fe73c6cd8ce47a3c6196c1f0731f0e
Lisa <110@holbetonschool.com>
2017-06-08 16:08:13 +0000

    adding console

--- console.py
+++ console.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 import cmd
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from models import storage
+
 """module: console.py"""
 
 
@@ -17,8 +21,57 @@ class HBNBCommand(cmd.Cmd):
         """Quit command to exit the program\n"""
         raise SystemExit
 
+    def do_create(self, args):
+        if not args:
+            print("** class name missing **")
+        else:
+            s = args.split()
+            if s[0] is None:
+                print("** class name missing **")
+            else:
+                if s[0] in classes:
+                    cls = classes[s[0]]()
+                    cls.save()
+                else:
+                    print("** class doesn't exist **")
+    def do_show(self, args):
+        if not args:
+            print("** class name missing **")
+        else:
+            s = args.split()
+
+            if not s[0]:
+                print("** class name missing **")
+            try:
+                s[1]
+            except Exception:
+                print("** instance id missing **")
+            else:
+                if s[0] in classes:
+                    obj = storage.all()
+                    print(obj[s[1]])
+                else:
+                    print("** class doesn't exist **")
+
+    def do_destroy(self, args):
+        if not args:
+            print("** class name missing **")
+        else:
+            s = args.split()
+            if not s[0]:
+                print("** class name missing **")
+            try:
+                s[1]
+            except Exception:
+                print("** instance id missing **")
+            else:
+                if s[0] in classes:
+                    obj = storage.all()
+                else:
+                    print("** class doesn't exist **")
     def emptyline(self):
         pass
 
 if __name__ == '__main__':
+    classes = {'BaseModel': BaseModel}
     HBNBCommand().cmdloop()
