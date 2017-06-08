95277536cca0f6d8850f08d182ef2e263e42ace3
Julija Lee <lee.julija@gmail.com>
2017-06-08 02:18:30 +0000

    fixed datetime foramt

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 import json
 import datetime
+import os.path
 
 class FileStorage:
+    dt_format = '%Y-%m-%dT%H:%M:%S.%f'
+
     def __init__(self):
         self.__file_path = './file.json'
         self.__objects = {}
@@ -19,18 +22,27 @@ class FileStorage:
         obj = self.__objects
         for i in self.__objects.keys():
             temp = self.__objects[i].to_json()
-        store[i] = temp
-        with open(self.__file_path, 'w+') as f:
+            store[i] = temp
+        with open(self.__file_path, "w+") as f:
             json.dump(store, f)
 
     def reload(self):
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path, 'r') as f:
+                r = json.load(f)
+            from models.base_model import BaseModel
+            for i in r.keys():
+                r[i]['created_at'] = datetime.datetime.strptime(r[i]['created_at'], self.dt_format)
+                try:
+                    r[i]["updated_at"] = datetime.datetime.strptime(r[i]["updated_at"], self.dt_format)
+                except:
+                    pass
+                self.__objects[i] = BaseModel(**r[i])
+            return self.__objects
+        else:
+            return {}
 
-        with open(self.__file_path, 'r') as f:
-            r = json.load(f)
-        from models.base_model import BaseModel
-        for i in r.keys():
-            self.__objects[i] = BaseModel(**r[i])
-        return self.__objects
+
         """
         return {"hello" : "world"}
         self.__objects[i] = eval(BaseModel)(r[i])
