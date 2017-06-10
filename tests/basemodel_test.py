68a94b73bc31da2a6a6e16ed2d3ddaf65ba36ff8
Lisa Leung <110@holbertonschool.com>
2017-06-10 02:44:10 +0000

    update

--- tests/basemodel_test.py
+++ tests/basemodel_test.py
@@ -10,3 +10,14 @@ class TestBaseModel(unittest.TestCase):
 
     def testattr(self):
         self.assertTrue(hasattr(self.basemodel, "created_at"))
+        self.assertTrue(hasattr(self.basemodel, "id"))
+        self.assertFalse(hasattr(self.basemodel, "updated_at"))
+        self.assertFalse(hasattr(self.basemodel, "random_attr"))
+        self.assertFalse(hasattr(self.basemodel, "name"))
+        self.basemodel.name = "Betty"
+        self.basemodel.age = 89
+        self.assertTrue(hasattr(self.basemodel, "name"))
+        self.assertEqual(self.basemodel.name, "Betty")
+        self.assertTrue(hasattr(self.basemodel, "age"))
+        delattr(self.basemodel, "name")
+        self.assertFalse(hasattr(self.basemodel, "name"))
