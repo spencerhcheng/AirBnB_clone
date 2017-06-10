03d5217ab23bfbe9c5cf5ac7869aa4b2529f00f8
Lisa Leung <110@holbertonschool.com>
2017-06-10 03:01:58 +0000

    update

--- tests/basemodel_test.py
+++ tests/basemodel_test.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 import unittest
-
 from models.base_model import BaseModel
+"""basemodel_test module"""
+
 
 class TestBaseModel(unittest.TestCase):
+    """Class: TestBaseModel"""
 
     def setUp(self):
+        """Setting up the instance(s)"""
         self.basemodel = BaseModel()
 
     def testattr(self):
+        """Testing the attributes of BaseModel"""
         self.assertTrue(hasattr(self.basemodel, "created_at"))
         self.assertTrue(hasattr(self.basemodel, "id"))
         self.assertFalse(hasattr(self.basemodel, "updated_at"))
@@ -21,3 +25,19 @@ class TestBaseModel(unittest.TestCase):
         self.assertTrue(hasattr(self.basemodel, "age"))
         delattr(self.basemodel, "name")
         self.assertFalse(hasattr(self.basemodel, "name"))
+        self.assertEqual(self.basemodel.__class__.__name__, "BaseModel")
+
+    def testmethod(self):
+        """Testing the methods of BaseModel"""
+        self.basemodel.save()
+        self.assertTrue(hasattr(self.basemodel, "updated_at"))
+
+    def teststr(self):
+        """Testing the str format of BaseModel"""
+        s = "[{}] ({}) {}".format(self.basemodel.__class__.__name__,
+                                  str(self.basemodel.id),
+                                  self.basemodel.__dict__)
+        self.assertEqual(print(s), print(self.basemodel))
+
+if __name__ == '__main__':
+    unittest.main()
