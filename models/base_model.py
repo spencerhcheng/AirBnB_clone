8145df87b53a175acc1cc4bf382a80d004a2308d
Lisa Leung <110@holbertonschool.com>
2017-06-11 05:36:15 +0000

    pep8 fix and doc

--- models/base_model.py
+++ models/base_model.py
@@ -3,8 +3,8 @@ import datetime
 import uuid
 import json
 from models import storage
-
 """module: class BaseModel"""
+
 
 class BaseModel:
     """class BaseModel"""
@@ -15,7 +15,8 @@ class BaseModel:
         if (kwargs.get('id') is not None):
             if 'created_at' in kwargs:
                 try:
-                    kwargs['created_at'] = datetime.datetime.strptime(kwargs['created_at'], self.dt_format)
+                    kwargs['created_at'] = datetime.datetime.strptime
+                    (kwargs['created_at'], self.dt_format)
                 except:
                     pass
             self.__dict__ = kwargs
@@ -32,13 +33,16 @@ class BaseModel:
 
     def to_json(self):
         """public instance method: to_json """
-        """creates a copy of self.__dict__ and update with atribute __class__"""
         new_dict = self.__dict__.copy()
-        new_dict.update({'created_at': self.created_at.strftime(self.dt_format)})
+        new_dict.update({'created_at': self.created_at.strftime
+                         (self.dt_format)})
         new_dict.update({'__class__': str(self.__class__.__name__)})
         if hasattr(self, 'updated_at'):
-            new_dict.update({'updated_at':self.updated_at.strftime(self.dt_format)})
+            new_dict.update({'updated_at': self.updated_at.strftime
+                             (self.dt_format)})
         return new_dict
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, str(self.id), self.__dict__)
+        """str overwirte"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, str(self.id),
+                                     self.__dict__)
