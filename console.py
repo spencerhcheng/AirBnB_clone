1c3ea8f3ab4558345b380725a0f75943e266bd66
Lisa Leung <110@holbertonschool.com>
2017-06-11 23:43:40 +0000

    adding Place

--- console.py
+++ console.py
@@ -6,6 +6,7 @@ from models.amenity import Amenity
 from models.state import State
 from models.user import User
 from models.review import Review
+from models.place import Place
 from models.engine.file_storage import FileStorage
 from models import storage
 
