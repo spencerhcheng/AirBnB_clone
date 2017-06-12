e9e5c0e0bec90cd5bdba047cdb9d721a14c4de2c
Julija Lee <lee.julija@gmail.com>
2017-06-12 19:06:25 +0000

    removed class from json dict

--- models/base_model.py
+++ models/base_model.py
@@ -13,12 +13,13 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """init"""
         if (kwargs.get('id') is not None):
-            if 'created_at' in kwargs:
-                try:
-                    kwargs['created_at'] = datetime.datetime.strptime(
-                        kwargs['created_at'], self.dt_format)
-                except:
-                    pass
+            if "__class__" in kwargs:
+                del kwargs["__class__"]
+            kwargs['created_at'] = datetime.datetime.strptime(
+                kwargs['created_at'], self.dt_format)
+            if "updated_at" in kwargs:
+                kwargs['updated_at'] = datetime.datetime.strptime(
+                    kwargs['updated_at'], self.dt_format)
             self.__dict__ = kwargs
         else:
             self.id = str(uuid.uuid4())
